c8e1b1623b349a15c2ced3ffb2b3ff61107ff407
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2024-05-06 13:28:20 +0600

    added main method

--- Merge_K_Sorted_Lists.cpp
+++ Merge_K_Sorted_Lists.cpp
@@ -1,57 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
 struct ListNode {
-  int val;
-  ListNode* next;
-  ListNode() : val(0), next(nullptr) {}
-  ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode* next) : val(x), next(next) {}
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode* next) : val(x), next(next) {}
 
 };
 
 class Solution {
+private:
+	ListNode* mergeTwoList(ListNode* first, ListNode* second) {
+		if (!first)
+			return second;
+		if (!second)
+			return first;
+		ListNode* answer = new ListNode(-1);
+		ListNode* temp = answer;
+		while (first && second) {
+			if (first->val < second->val) {
+				temp->next = first;
+				temp = temp->next;
+				first = first->next;
+			} else {
+				temp->next = second;
+				temp = temp->next;
+				second = second->next;
+			}
+		}
+		while (first) {
+			temp->next = first;
+			temp = temp->next;
+			first = first->next;
+		}
+		while (second) {
+			temp->next = second;
+			temp = temp->next;
+			second = second->next;
+		}
+		return answer->next;
+	}
 public:
-  ListNode* mergeTwoList(ListNode* first, ListNode* second) {
-    if (!first)
-      return second;
-    if (!second)
-      return first;
-    ListNode* answer = new ListNode(-1);
-    ListNode* temp = answer;
-    while (first && second) {
-      if (first->val < second->val) {
-        temp->next = first;
-        temp = temp->next;
-        first = first->next;
-      } else {
-        temp->next = second;
-        temp = temp->next;
-        second = second->next;
-      }
-    }
-    while (first) {
-      temp->next = first;
-      temp = temp->next;
-      first = first->next;
-    }
-    while (second) {
-      temp->next = second;
-      temp = temp->next;
-      second = second->next;
-    }
-    return answer->next;
-  }
+	ListNode* mergeKLists(vector<ListNode*>& lists) {
+		if (lists.size() == 0) {
+			return NULL;
+		}
+		ListNode* head = lists[0];
 
-  ListNode* mergeKLists(vector<ListNode*>& lists) {
-    if (lists.size() == 0) {
-      return NULL;
-    }
-    ListNode* head = lists[0];
+		for (int i = 1; i < lists.size(); i++) {
+			head = mergeTwoList(head, lists[i]);
+		}
 
-    for (int i = 1; i < lists.size(); i++) {
-      head = mergeTwoList(head, lists[i]);
-    }
-
-    return head;
-  }
+		return head;
+	}
 };
+
+int main() {
+
+}
